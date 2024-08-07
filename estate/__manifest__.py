b90f5e64ff5981ee54a8d4936035828f795ce16a
Lukas Holm <lukas@vkdata.dk>
2024-08-07 14:36:20 +0200

    [IMP] base.group_user now has all access rights to estate.property

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': "Real Estate",
-    'version': '17.0.1.1.1',
+    'version': '17.0.1.2.0',
     'license': 'OPL-1',
     'author': "VK DATA ApS",
     'depends': ['base'],
@@ -8,4 +8,9 @@
     'category': 'Sales',
     'description': 'The Real Estate module provides tools to list Estate properties, like the price, features and availability of the real estate',
     'application': True,
+
+    'data': [
+        'security/ir.model.access.csv'
+    ]
+
 }
