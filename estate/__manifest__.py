da98b2172d877164a58fa80f9e35ba8a29ff8f5c
Lukas Holm <lukas@vkdata.dk>
2024-08-07 11:15:31 +0200

    [ADD] Real Estate module - initial commit added __init__.py and __manifest__.py required by Odoo. Filled out the manifest with info required by the guidelines.

---
+++ estate/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': "Real Estate",
+    'version': '17.0.1.0.0',
+    'license': 'OPL-1',
+    'author': "VK DATA ApS",
+    'depends': ['base'],
+    'summary': 'A module designed to list real estate and it\'s properties',
+    'category': 'Sales',
+    'description': 'The Real Estate module provides tools to list Estate properties, like the price, features and availability of the real estate',
+    'application': True,
+}
