e648e276ba7f6e602cd06d481d79303afe3af5fd
Lukas Holm <lukas@vkdata.dk>
2024-08-13 10:31:53 +0200

    [IMP] add security groups and rules real estate agent can now only see and interact with properties they themselves are selling.

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,21 +1,25 @@
 {
     'name': "Real Estate",
-    'version': '17.0.1.25.4',
+    'version': '17.0.1.26.0',
     'license': 'OPL-1',
     'author': "VK DATA ApS",
     'depends': ['base'],
     'summary': 'A module designed to list real estate and it\'s properties',
-    'category': 'Sales',
+    'category': 'Real Estate/Brokerage',
     'description': 'The Real Estate module provides tools to list Estate properties, like the price, features and availability of the properties',
     'application': True,
     'data': [
-        'security/ir.model.access.csv',
         'views/estate_property_views.xml',
         'views/estate_property_tag_views.xml',
         'views/estate_property_offer_views.xml',
         'views/estate_property_type_views.xml',
         'views/res_users_views.xml',
         'views/estate_menus.xml',
+
+        'security/estate_groups.xml',
+        'security/ir.model.access.csv',
+        'security/estate_property_security.xml',
+
     ]
 
 }
