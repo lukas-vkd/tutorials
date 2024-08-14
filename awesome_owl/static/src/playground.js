87e41cf0f722dcec805a0fca289f5f7cb2258290
Lukas Holm <lukas@vkdata.dk>
2024-08-14 14:16:33 +0200

    [IMP] add card component

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -2,10 +2,12 @@
 
 import { Component } from "@odoo/owl";
 import { Counter } from "./counter/counter";
+import { Card } from "./card/card";
+
 
 export class Playground extends Component {
     static template = "awesome_owl.playground";
 
 
-    static components = { Counter };
+    static components = { Counter, Card };
 }
