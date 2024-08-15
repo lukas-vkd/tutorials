bc588f0672afb46bbe2ab4ae2f4a68ec76aa2b83
Lukas Holm <lukas@vkdata.dk>
2024-08-15 13:48:32 +0200

    [IMP] add piechart that shows shirt order by size

--- awesome_dashboard/static/src/dashboard.js
+++ awesome_dashboard/static/src/dashboard.js
@@ -4,9 +4,12 @@ import { registry } from "@web/core/registry";
 import { Layout } from "@web/search/layout";
 import { useService } from "@web/core/utils/hooks";
 import { DashboardItem } from "./dashboard_item/dashboard_item";
+import { PieChart } from "./pie_chart/pie_chart";
+
 class AwesomeDashboard extends Component {
     static template = "awesome_dashboard.AwesomeDashboard";
     static components = { Layout, DashboardItem };
+    static components = { Layout, DashboardItem, PieChart };
 
     setup() {
         this.action = useService("action");
@@ -18,7 +21,6 @@ class AwesomeDashboard extends Component {
             this.statistics = await this.statistics.loadStatistics();
         });
     }
-
     openCustomerView() {
         this.action.doAction("base.action_partner_form");
     }
