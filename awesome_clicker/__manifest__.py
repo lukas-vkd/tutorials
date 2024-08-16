b233051c842d91b292d5871ea41f4be09032b243
Lukas Holm <lukas@vkdata.dk>
2024-08-16 12:44:42 +0200

    [IMP] made clicker count independent of ui

--- awesome_clicker/__manifest__.py
+++ awesome_clicker/__manifest__.py
@@ -13,7 +13,7 @@
     'author': "Odoo",
     'website': "https://www.odoo.com/",
     'category': 'Tutorials/AwesomeClicker',
-    'version': '17.0.0.2.0',
+    'version': '17.0.0.3.0',
     'application': True,
     'installable': True,
     'depends': ['base', 'web'],
