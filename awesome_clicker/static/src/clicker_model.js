8738996306fdc891347751eacf0235bb02ce646d
Lukas Holm <lukas@vkdata.dk>
2024-08-19 09:52:54 +0200

    [IMP] add bigbots

--- awesome_clicker/static/src/clicker_model.js
+++ awesome_clicker/static/src/clicker_model.js
@@ -1,6 +1,7 @@
 /** @odoo-module */
 
 import { Reactive }  from "@web/core/utils/reactive";
+import { EventBus } from "@odoo/owl";
 
 export class ClickerModel extends Reactive {
     constructor() {
@@ -8,16 +9,28 @@ export class ClickerModel extends Reactive {
         this.clicks = 0;
         this.level = 0;
         this.clickBots = 0;
+        this.bigbots = 0;
+        this.bus = new EventBus();
+
 
         document.addEventListener("click", () => this.increment(1), true);
         setInterval(() => {
             this.clicks += this.clickBots * 10;
+            this.clicks += this.bigbots * 100;
         }, 10000);
+
+
+        
     }
 
     increment(inc) {
         this.clicks += inc;
         if (this.level < 1 && this.clicks >= 1000) {
+            this.bus.trigger("MILESTONE_1k");
+            this.level++;
+        }
+        if (this.level < 2 && this.clicks >= 5000) {
+            this.bus.trigger("MILESTONE_5k");
             this.level++;
         }
     }
@@ -29,5 +42,15 @@ export class ClickerModel extends Reactive {
         }
         this.clicks -= clickBotPrice;
         this.clickBots += 1;
+    }
+
+
+    buyBigBot() {
+        const bigBotPrice = 5000;
+        if (this.clicks < bigBotPrice) {
+            return false;
+        }
+        this.clicks -= bigBotPrice;
+        this.bigbots += 1;
     }
 }
--- awesome_clicker/static/src/clicker_service.js
+++ awesome_clicker/static/src/clicker_service.js
@@ -1,10 +1,25 @@
 /** @odoo-module */
 import { registry } from "@web/core/registry";
 import { ClickerModel } from "./clicker_model";
-
+import { useService } from "@web/core/utils/hooks";
 const clickerService = {
-    start(env) {
-        return new ClickerModel();
+    dependencies: ["effect"],
+    start(env, services) {
+        const clickerModel = new ClickerModel();
+        const bus = clickerModel.bus;
+        bus.addEventListener("MILESTONE_1k", () => {
+            services.effect.add({
+                message: "Milestone reached! You can now buy clickbots",
+                type: "rainbow_man",
+            });
+        });
+        bus.addEventListener("MILESTONE_5k", () => {
+            services.effect.add({
+                message: "Milestone reached! You can now buy bigbots",
+                type: "rainbow_man",
+            });
+        });
+        return clickerModel;
     },
 };
 
